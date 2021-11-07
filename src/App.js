ee23ba065fec1b1393dd0c9dc142d3164ad61d7d
starwalker00 <aaronwalkerup@gmail.com>
2021-11-07 21:19:58 +0100

    app: improve visibility

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import React, { useState } from 'react';
 import QrReader from 'react-qr-reader'
 
 function App() {
-  const [result, setResult] = useState('No result');
-  
+  const defaultResult = '0x0561BCf470ea1837fB3c867b11fAd8E6749F35af;test;0x1bd6133900f4d1ade6281e99e73be30e6442104479f5c01e8bb8873adb6531235ac45df506dd283b92ada2538bd868f8762eb78e9de243e23b965f28f886c10d1c'
+  // const defaultResult = 'No result'
+  const [result, setResult] = useState(defaultResult)
+
+  // manage displayed elements from result
+  const resultHasExpectedSize = result.split(';').length === 3
+  let listItems = null
+  if (resultHasExpectedSize) {
+    listItems = result.split(';').map((result) =>
+      <li>{result}</li>
+      );
+  }
+
+  // QR code reader handlers
   function handleScan(data) {
     if (data) {
       setResult(data)
@@ -12,6 +24,7 @@ function App() {
   function handleError(err) {
     console.error(err)
   }
+
   return (
     <div>
     <QrReader
@@ -21,6 +34,10 @@ function App() {
       style={{ width: '100%' }}
     />
     <p>{result}</p>
+    {resultHasExpectedSize
+        ? <p><ul>{listItems}</ul></p>
+        : <p>'Badly formatted QR code content'</p>
+    }
     </div>
   );
 }
