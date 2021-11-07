496500333e9ab2ff34ac8d41093d274f36e2a6a8
starwalker00 <aaronwalkerup@gmail.com>
2021-11-07 23:02:49 +0100

    app: add multi-route

--- src/App.js
+++ src/App.js
@@ -1,71 +1,18 @@
-import React, { useState } from 'react';
-import QrReader from 'react-qr-reader'
-import { ethers } from 'ethers'
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Navigation, Footer, Home, About, Check } from "./components";
 function App() {
-  // const defaultResult = '0x0561BCf470ea1837fB3c867b11fAd8E6749F35af;test;0x1bd6133900f4d1ade6281e99e73be30e6442104479f5c01e8bb8873adb6531235ac45df506dd283b92ada2538bd868f8762eb78e9de243e23b965f28f886c10d1c'
-  const defaultResult = 'No result'
-  const [result, setResult] = useState(defaultResult)
-
-  // manage displayed elements from result
-  const resultHasExpectedSize = result.split(';').length === 3
-  console.log(`resultHasExpectedSize: ${resultHasExpectedSize}`)
-
-  let listItems = null
-  let isSignatureVerified = false
-  let isSignatureValid = null
-  if (resultHasExpectedSize) {
-    console.log(`result: ${result}`)
-    let resultArray = result.split(';')
-    console.dir(`resultArray: ${resultArray}`)
-    let address = resultArray[0]
-    console.dir(`address: ${address}`)
-    let message = resultArray[1]
-    console.dir(`message: ${message}`)
-    let signature = resultArray[2]
-    console.dir(`signature: ${signature}`)
-    listItems = [
-      <ul>
-        <li>{address}</li>
-        <li>{message}</li>
-        <li>{signature}</li>
-      </ul>
-    ];
-      let signerAddress = ethers.utils.verifyMessage(message , signature)
-      console.log(signerAddress)
-      isSignatureValid = signerAddress === address
-      isSignatureVerified = true
-  }
-
-  // QR code reader handlers
-  function handleScan(data) {
-    if (data) {
-      setResult(data)
-    }
-  }
-  function handleError(err) {
-    console.error(err)
-  }
-
   return (
-    <div>
-    <QrReader
-      delay={300}
-      onError={(err) => handleError(err)}
-      onScan={(data) => handleScan(data)}
-      style={{ width: '100%' }}
-    />
-    <p>{result}</p>
-    {resultHasExpectedSize
-      ? <div>{listItems}</div>
-      : <div>'Badly formatted QR code content'</div>
-    }
-    { isSignatureVerified 
-        ? isSignatureValid 
-          ? <div>SignatureValid : YES</div>
-          : <div>SignatureValid : NO</div>
-        : <div>Signature not verified</div>
-    }
+    <div className="App">
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route path="/" exact component={() => <Home />} />
+          <Route path="/check" exact component={() => <Check />} />
+          <Route path="/about" exact component={() => <About />} />
+        </Switch>
+        {/* <Footer /> */}
+      </Router>
     </div>
   );
 }
